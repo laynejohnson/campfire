db091ec1012af2b9bfcfe62c627d7c1dbecc9d8c
Layne Johnson <layne@findum.com>
2021-07-28 10:54:39 -0400

    Welcome storyboard sb initial config

--- Bonfire/Controllers/WelcomeViewController.swift
+++ Bonfire/Controllers/WelcomeViewController.swift
@@ -5,9 +5,28 @@
 //  Created by Layne Johnson on 7/28/21.
 //  Copyright © 2021. All rights reserved.
 
+// MARK: - Development List:
+
+// ---------------------------------- //
+// - - - - - - - - DEV - - - - - - -  //
+// ---------------------------------- //
+
 import UIKit
 
 class WelcomeViewController: UIViewController {
+    
+    // MARK: - IBOutlets
+    
+    // ---------------------------------- //
+    // - - - - - - -  OUTLETS - - - - - - //
+    // ---------------------------------- //
+    
+    
+    // MARK: - Variables
+    
+    // ---------------------------------- //
+    // - - - - - - - - VARS - - - - - - - //
+    // ---------------------------------- //
 
     override func viewDidLoad() {
         super.viewDidLoad()
