12946c793d3f2971011dd3c374c142e14646c5bc
Layne Johnson <layne@findum.com>
2021-07-28 19:03:08 -0400

    Build chat vc initial config

--- Bonfire/Controllers/ChatViewController.swift
+++ Bonfire/Controllers/ChatViewController.swift
@@ -9,13 +9,19 @@ import UIKit
 
 class ChatViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var messageTextField: UITextField!
+    @IBOutlet weak var sendButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func sendPressed(_ sender: Any) {
+    }
+    
     /*
     // MARK: - Navigation
 
