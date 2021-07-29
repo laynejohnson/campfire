72021caddba8b6aebfaf8e5ec2eebfde4920fda0
Layne Johnson <layne@findum.com>
2021-07-29 07:55:36 -0400

    Add join segue

--- Bonfire/Controllers/RegisterViewController.swift
+++ Bonfire/Controllers/RegisterViewController.swift
@@ -13,7 +13,7 @@ class RegisterViewController: UIViewController {
 
     @IBOutlet weak var emailField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
-    @IBOutlet weak var registerButton: UIButton!
+    @IBOutlet weak var joinButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,15 +21,16 @@ class RegisterViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    @IBAction func joinPressed(_ sender: UIButton) {
+        
+         performSegue(withIdentifier: "joins", sender: self)
+        
+    }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
-    */
+
 
 }
