8090f47ea8eb97c44485d6623f0b891919526d8d
Layne Johnson <layne@findum.com>
2021-07-29 12:01:51 -0400

    Edit .gitignore

--- Bonfire/AppDelegate.swift
+++ Bonfire/AppDelegate.swift
@@ -6,8 +6,6 @@
 //  Copyright © 2021. All rights reserved.
 
 import UIKit
-import Firebase
-
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,8 +13,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        FirebaseApp.configure
-        
         return true
     }
 
