52ab52db39e604d873ac9fbb7543293e72a97829
Layne Johnson <layne@findum.com>
2021-07-30 09:04:30 -0400

    Implement login

--- Bonfire/Controllers/LoginViewController.swift
+++ Bonfire/Controllers/LoginViewController.swift
@@ -6,9 +6,10 @@
 //  Copyright © 2021. All rights reserved.
 
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
-
+    
     @IBOutlet weak var emailField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
     @IBOutlet weak var loginButton: UIButton!
@@ -17,22 +18,36 @@ class LoginViewController: UIViewController {
         super.viewDidLoad()
         
         
-
-     
+        
+        
     }
     
     
     @IBAction func loginPressed(_ sender: UIButton) {
         
-        performSegue(withIdentifier: "LoginChat", sender: self)
+        if let email = emailField.text, let password = passwordField.text {
+            
+            Auth.auth().signIn(withEmail: email, password: password) { [weak self] authResult, error in
+                guard let strongSelf = self else { return }
+                
+                if let e = error {
+                    print(e.localizedDescription)
+                } else {
+                    print("User logged in successfuly.")
+                    self?.performSegue(withIdentifier: "LoginChat", sender: self)
+                }
+            }
+            
+        }
+        
     }
     
-
-
+    
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
     
-
+    
 }
