15875df4b396e6c7b30eff571ba057c62df39c05
Layne Johnson <layne@findum.com>
2021-08-02 15:57:35 -0400

    Initialize database, implement load messages function

--- Bonfire/AppDelegate.swift
+++ Bonfire/AppDelegate.swift
@@ -15,6 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     
         FirebaseApp.configure()
+
+        let db = Firestore.firestore()
+        
         return true
     }
 
--- Bonfire/Controllers/ChatViewController.swift
+++ Bonfire/Controllers/ChatViewController.swift
@@ -11,6 +11,28 @@ import Firebase
 
 // TODO: Implement MessageKit: https://cocoapods.org/pods/MessageKit
 // TODO: Add fire to nav bar
+// TODO: Add user features
+
+/*
+ 
+ let user = Auth.auth().currentUser
+ if let user = user {
+ // The user's ID, unique to the Firebase project.
+ // Do NOT use this value to authenticate with your backend server,
+ // if you have one. Use getTokenWithCompletion:completion: instead.
+ let uid = user.uid
+ let email = user.email
+ let photoURL = user.photoURL
+ var multiFactorString = "MultiFactor: "
+ for info in user.multiFactor.enrolledFactors {
+ multiFactorString += info.displayName ?? "[DispayName]"
+ multiFactorString += " "
+ }
+ // ...
+ }
+ */
+
+
 
 class ChatViewController: UIViewController {
     
@@ -18,12 +40,12 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var messageTextField: UITextField!
     @IBOutlet weak var sendButton: UIButton!
     
+    let db = Firestore.firestore()
+    
     var messages: [Message] = [
-        Message(sender: Constants.testChatter1, body: "Good day friend!"),
-        Message(sender: Constants.testChatter2, body: "Good day to you, my friend!"),
-        Message(sender: Constants.testChatter1, body: "Meet you by the bonfire??")
+     
     ]
-        
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,11 +58,40 @@ class ChatViewController: UIViewController {
         // Dismiss keyboard with tap gesture
         let tapGesture = UITapGestureRecognizer(target: view, action: #selector(UIView.endEditing))
         view.addGestureRecognizer(tapGesture)
-
+        
         // If gesture blocks other touches
         // tapGesture.cancelsTouchesInView = false
         
         tableView.register(UINib(nibName: Constants.cellNibName, bundle: nil), forCellReuseIdentifier: Constants.cellIdentifier)
+        
+        loadMessages()
+    }
+    
+    func loadMessages() {
+        
+        messages = []
+        db.collection(Constants.FStore.collectionName).getDocuments { querySnapshot, error in
+            if let e = error {
+                print("There was a problem retrieving data from Firestore: \(e)")
+            } else {
+                if let snapshotDocuments = querySnapshot?.documents {
+                    for doc in snapshotDocuments {
+                        let data = doc.data()
+                        if let sender = data[Constants.FStore.senderField] as? String, let messageBody = data[Constants.FStore.bodyField] as? String {
+                            let newMessage = Message(sender: sender, body: messageBody)
+                            self.messages.append(newMessage)
+                            
+                            // Fetch main thread:
+                            // Process happens in a closure (process happens in background); main thread must be fetched (process happening in foreground
+                            DispatchQueue.main.async {
+                                self.tableView.reloadData()
+                            }
+                            
+                        }
+                    }
+                }
+            }
+        }
     }
     
     @IBAction func logOutPressed(_ sender: UIBarButtonItem) {
@@ -59,26 +110,38 @@ class ChatViewController: UIViewController {
     
     @IBAction func sendPressed(_ sender: Any) {
         
+        if let messageBody = messageTextField.text, let messageSender = Auth.auth().currentUser?.email {
+            
+            db.collection(Constants.FStore.collectionName).addDocument(data: [
+                Constants.FStore.senderField: messageSender,
+                Constants.FStore.bodyField: messageBody
+            ]) { (error) in
+                if let e = error {
+                    print("There was a problem saving data to Firestore: \(e)")
+                } else {
+                    print("Data saved successfully")
+                }
+            }
+        }
     }
+} // End ChatViewController
     
-}
-
-extension ChatViewController: UITableViewDataSource {
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-
-        return messages.count
+    extension ChatViewController: UITableViewDataSource {
+        
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            
+            return messages.count
+        }
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            
+            let cell = tableView.dequeueReusableCell(withIdentifier: Constants.cellIdentifier, for: indexPath) as! MessageCell
+            
+            cell.bubbleLabel.textColor = UIColor.black
+            cell.bubbleLabel.text = messages[indexPath.row].body
+            
+            return cell
+        }
+        
+        
     }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        let cell = tableView.dequeueReusableCell(withIdentifier: Constants.cellIdentifier, for: indexPath) as! MessageCell
-        
-        cell.bubbleLabel.textColor = UIColor.black
-        cell.bubbleLabel.text = messages[indexPath.row].body
-        
-        return cell
-    }
-    
-    
-}
